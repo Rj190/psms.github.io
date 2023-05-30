49ddb5d903ccdd9c2a4a00f8c4e4a8c102db54db
Rohit Jadhav <46531471+Rj190@users.noreply.github.com>
2023-05-30 18:10:00 +0530

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,9 +1,9 @@
 <?php
   
-  $server="aws.connect.psdb.cloud";
-  $user="l3mp9gsvix0i5o2pex37";
-  $password="pscale_pw_tt0hUdsK0zd65Ev4wTQeRO46T29CcgogLhcCkDsTt0D";
- $dbname="smart_solutions";
+  $server="sql303.epizy.com";
+  $user="epiz_31255204";
+  $password="4xcEbSr5crhjTm";
+ $dbname="epiz_31255204_smart_solutions";
  $con=mysqli_connect($server,$user,$password);
   mysqli_select_db($con,$dbname);
    
