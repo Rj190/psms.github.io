b9e537d2a1b3d622644a3587a60e392173964ba0
Rohit Jadhav <46531471+Rj190@users.noreply.github.com>
2023-05-30 17:17:45 +0530

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,8 +1,8 @@
 <?php
   
-  $server="localhost";
-  $user="root";
-  $password="";
+  $server="aws.connect.psdb.cloud";
+  $user="hkzrqby43n2xy3ktak23";
+  $password="pscale_pw_rXaPTwGPSr2rQWbw6HFpNkCMClQlT7Coz8HT4srgajL";
  $dbname="smart_solutions";
  $con=mysqli_connect($server,$user,$password);
   mysqli_select_db($con,$dbname);
